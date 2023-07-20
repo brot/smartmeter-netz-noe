5e4509294ccd4071cc2f7a216fbc7df3304c89c5
Bernd Schlapsi <dev@bschlapsi.at>
2023-07-20 21:58:27 +0200

    Update python dependencies

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
 import datetime
 import pathlib
 
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    username: str = Field(..., env="WEB_PORTAL_USERNAME")
-    password: str = Field(..., env="WEB_PORTAL_PASSWORD")
+    username: str = Field(..., validation_alias="WEB_PORTAL_USERNAME")
+    password: str = Field(..., validation_alias="WEB_PORTAL_PASSWORD")
     measure_start_date: datetime.date
     storage_path: pathlib.Path
     user_agent: str
